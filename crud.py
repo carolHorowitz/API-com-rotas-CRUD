d5166d7f45458341a43bbf6a8eeb18412e7e058f
carolHorowitz <carolvet.horowitz@gmail.com>
2022-12-07 16:32:55 -0300

    Detalhando funcionalidades CRUD em Python

--- crud.py
+++ crud.py
@@ -9,6 +9,33 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
+# CRUD - etapas
+
+# CREATE
+
+# produto = "pulseira"
+# quantidade = 5
+# cursor.execute(f'INSERT INTO estoque (produto, quantidade) VALUES ("{produto}", {quantidade})')
+# conexao.commit() #usado para editar BD (create, update ou delete)
+
+# READ
+
+# cursor.execute(f'SELECT * FROM estoque')
+# resultado = cursor.fetchall() #ler o banco de dados
+# print(resultado)
+
+# UPDATE
+
+# produto = "colar"
+# quantidade = 10
+# cursor.execute(f'UPDATE estoque SET quantidade = {quantidade} WHERE produto = "{produto}"')
+# conexao.commit()
+
+# DELETE
+
+# produto = "pulseira"
+# cursor.execute(f'DELETE FROM estoque WHERE produto = "{produto}"')
+# conexao.commit()
 
 
 cursor.close()
